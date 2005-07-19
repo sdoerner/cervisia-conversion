73f3a407b02feab849c36b42e1ad0caa7004d676
Christian Loose <christian.loose@hamburg.de>
2005-07-19 21:12:16 +0000

    new base class for svn command classes
    svn path=/branches/cervisia/subversion_support/kdesdk/cervisia/; revision=436494

---
+++ plugins/svnplugin/svncommandbase.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2005 Christian Loose <christian.loose@kdemail.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "svncommandbase.h"
+using Cervisia::SvnCommandBase;
+
+#include <dcopref.h>
+#include <svnjob_stub.h>
+
+#include <kdebug.h>
+
+
+SvnCommandBase::SvnCommandBase(const ActionKind& action)
+    : CommandBase(action)
+    , m_svnJob(0)
+{
+    kdDebug(8050) << "SvnCommandBase::SvnCommandBase()" << endl;
+}
+
+
+SvnCommandBase::~SvnCommandBase()
+{
+    delete m_svnJob;
+}
+
+
+QString SvnCommandBase::commandString() const
+{
+    return m_svnJob->command();
+}
+
+
+void SvnCommandBase::execute()
+{
+    kdDebug(8050) << "SvnCommandBase::execute()" << endl;
+    m_svnJob->execute();
+}
+
+
+void SvnCommandBase::dcopJobExited(bool normalExit, int exitStatus)
+{
+    kdDebug(8050) << "SvnCommandBase::dcopJobExited(): normalExit = " << normalExit << endl;
+
+    emit jobExited(normalExit, exitStatus);
+    deleteLater();  //TODO: Okay?
+}
+
+
+void SvnCommandBase::dcopReceivedStdout(QString buffer)
+{
+    kdDebug(8050) << "SvnCommandBase::dcopReceivedStdout(): buffer = " << buffer << endl;
+
+    processOutput(buffer);
+    emit receivedStdout(buffer);
+}
+
+
+void SvnCommandBase::dcopReceivedStderr(QString buffer)
+{
+    kdDebug(8050) << "SvnCommandBase::dcopReceivedStderr(): buffer = " << buffer << endl;
+
+    processOutput(buffer);
+    emit receivedStderr(buffer);
+}
+
+
+void SvnCommandBase::connectToJob(const DCOPRef& jobRef)
+{
+    m_svnJob = new SvnJob_stub(jobRef);
+
+    // establish connections to the signals of the job
+    connectDCOPSignal(jobRef.app(), jobRef.obj(), "jobExited(bool, int)",
+                      "dcopJobExited(bool, int)", true);
+    connectDCOPSignal(jobRef.app(), jobRef.obj(), "receivedStdout(QString)",
+                      "dcopReceivedStdout(QString)", true);
+    connectDCOPSignal(jobRef.app(), jobRef.obj(), "receivedStderr(QString)",
+                      "dcopReceivedStderr(QString)", true);
+}
+
+
+void SvnCommandBase::processOutput(const QString& buffer)
+{
+    m_lineBuffer += buffer;
+
+    int pos;
+    while( (pos = m_lineBuffer.find('\n')) != -1 )
+    {
+        QString line = m_lineBuffer.left(pos);
+        if( !line.isEmpty() )
+            emit receivedLine(line);
+
+        m_lineBuffer = m_lineBuffer.right(m_lineBuffer.length()-pos-1);
+    }
+}
+
+#include "svncommandbase.moc"
