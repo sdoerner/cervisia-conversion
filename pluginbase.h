339b8ef1b7455c9d2957031170910b1d8127ae2c
Christian Loose <christian.loose@hamburg.de>
2005-08-09 22:44:18 +0000

    remove old PluginJobBase code
    svn path=/branches/cervisia/subversion_support/kdesdk/cervisia/; revision=444373

--- pluginbase.h
+++ pluginbase.h
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifndef CERVISIA_PLUGINBASE_H
@@ -31,7 +31,6 @@ namespace Cervisia
 
 class CommandBase;
 class IgnoreFilterBase;
-class PluginJobBase;
 class SelectionIntf;
 class UpdateParser;
 
@@ -66,16 +65,14 @@ public:
     virtual UpdateParser* updateParser() const = 0;
 
     virtual void annotate(const QString& fileName, 
-		          const QString& revision = QString::null) = 0;
+                          const QString& revision = QString::null) = 0;
 
 signals:
     void updateItem(const Cervisia::Entry& entry);
-    void jobPrepared(Cervisia::PluginJobBase* job);
     void commandPrepared(Cervisia::CommandBase* cmd);
 
 protected:
     SelectionIntf* m_fileView;
-    PluginJobBase* m_currentJob;
 };
 
 
