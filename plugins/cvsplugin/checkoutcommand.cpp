d44d5996e74b0bd209179be239c2a554456a6f96
Christian Loose <christian.loose@hamburg.de>
2006-01-09 19:00:51 +0000

    add checkout command classes for cvs plugin
    ATM "tag" and "alias" options are missing.
    
    
    svn path=/branches/cervisia/subversion_support/kdesdk/cervisia/; revision=496106

---
+++ plugins/cvsplugin/checkoutcommand.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2006 Christian Loose <christian.loose@kdemail.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "checkoutcommand.h"
+using Cervisia::CheckoutCommand;
+
+#include <dcopref.h>
+
+#include <qapplication.h>
+
+#include <cvsservice_stub.h>
+
+#include "cvsplugin.h"
+
+#include <kdebug.h>
+
+CheckoutCommand::CheckoutCommand(const QString& workingFolder, const QString& repository,
+                                 const QString& module)
+    : CvsCommandBase(Checkout)
+    , m_workingFolder(workingFolder)
+    , m_repository(repository)
+    , m_module(module)
+    , m_pruneDirectories(false)
+    , m_exportOnly(false)
+{
+}
+
+
+CheckoutCommand::~CheckoutCommand()
+{
+}
+
+
+void CheckoutCommand::setPruneDirectories(bool pruneDirs)
+{
+    m_pruneDirectories = pruneDirs;
+}
+
+
+void CheckoutCommand::setExportOnly(bool exportOnly)
+{
+    m_exportOnly = exportOnly;
+}
+
+
+bool CheckoutCommand::prepare()
+{
+    kdDebug(8050) << k_funcinfo << endl;
+
+    DCOPRef jobRef = CvsPlugin::cvsService()->checkout(m_workingFolder, m_repository,
+                                                       m_module, "" /*tag*/, m_pruneDirectories,
+                                                       "" /*alias*/, m_exportOnly, isRecursive());
+    connectToJob(jobRef);
+
+    return true;
+}
